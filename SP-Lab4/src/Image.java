320b098c79ab85107953814c50cd42ede914b478
MariusConstantinescu10 <constantinescumarius983@gmail.com>
2021-11-02 14:57:23 +0200

    Laborator 4

---
+++ SP-Lab4/src/Image.java
@@ -0,0 +1,41 @@
+import java.util.concurrent.TimeUnit;
+
+public class Image implements Element, Picture {
+    private String imageName;
+    private Dimension dim = new Dimension(400,400);
+
+    public Image(String imageName) {
+        this.imageName = imageName;
+
+        try {
+            TimeUnit.SECONDS.sleep(5);
+        }
+        catch( InterruptedException e) { e.printStackTrace(); }
+    }
+
+    public Image(Image image) {
+        this.imageName = image.imageName;
+    }
+
+    public void print() {
+        System.out.println(this.imageName);
+    }
+
+    @Override
+    public void add(Element element) {
+    }
+
+    @Override
+    public void remove(Element element) {
+    }
+
+    @Override
+    public String url() {
+        return this.imageName;
+    }
+
+    @Override
+    public Dimension dim() {
+        return this.dim;
+    }
+}
