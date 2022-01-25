a22d7ee851084d75469ca4a3c65311cb74e8157e
MariusConstantinescu10 <constantinescumarius983@gmail.com>
2022-01-25 21:32:50 +0200

    Laborator 6

---
+++ SP-Lab6/src/models/Element.java
@@ -0,0 +1,7 @@
+package models;
+
+public interface Element {
+    void print();
+    void add(Element element) throws Exception;
+    void remove(Element element);
+}
---
+++ SP-Lab6/src/services/SubChapter.java
@@ -0,0 +1,26 @@
+package services;
+
+import models.Element;
+
+import java.util.ArrayList;
+
+public class SubChapter {
+    private String name;
+    private ArrayList<Element> elements;
+
+    public SubChapter(String name) {
+        this.name = name;
+        this.elements = new ArrayList<Element>();
+    }
+
+    public void addElement (Element element) {
+        this.elements.add(element);
+    }
+
+    public void print() {
+        System.out.println("Subcapitol " + this.name);
+
+        for (Element i : this.elements)
+            i.print();
+    }
+}
